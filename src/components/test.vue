5835dc0640704d97d887e6b1dc06b8896fc6a0d4
Myriam <myriam@ctoutvert.com>
2023-10-11 16:16:27 +0200

    BotanicAPP | ajout composable

---
+++ src/components/test.vue
@@ -0,0 +1,23 @@
+<template class="mt-2">
+    Test Composant composable
+    c : {{ c }}
+
+    <v-btn @click="dec">-</v-btn>
+    count :  {{ c }}
+    <v-btn @click="inc">+</v-btn>
+
+
+</template>
+
+<script>
+    import composable from "../_composables/composable";
+
+    export default {
+        data(){
+            const {c, add, dec} = composable();
+            return {
+                c,add, dec
+            }
+        }
+    }
+</script>
